6abf507ac5b18762d2890d5909aecc3c0d913b9a
trent308 <trenton.brownlee@bobcats.gcsu.edu>
2021-02-19 17:41:16 -0500

    Update Friday 2/19/2021

--- reservationweb/htdocs/pages/roomRes.php
+++ reservationweb/htdocs/pages/roomRes.php
@@ -15,52 +15,99 @@
     .centerForm
     {
       text-align: center;
-      margin-top: 100px;
+      margin: auto;
+    }
+
+    body
+    {
+      background-color: #1C4F9C;
+    }
+    .containOutput
+    {
+      margin: auto;
+      width: 30%;
+    }
+    .centerContainedOutput
+    {
+      margin: 1vh;
+      border: 5px solid #1C5438;
+      padding: 1vh;
+      background-color: white;
+      width: 550px;
+      height: 550px;
+      text-align: center;
+    }
+    .redFont
+    {
+      color:red;
     }
   </style>  
+  <?php
+    include_once('default.php');
+    defaultHeader();
+    $roomSelected = "";
+    $location = "";
+    $numOfLocation = "";
+  ?>
   <body>
-    <div class="centerForm" style="text-align: center">
-      <h1>Reserve A Room Below</h1>
-      <form action="roomResData.php">
-        <!--This is the section for the form input
-          that will be sent to a page called
-          roomResData.php--->
-        <label for="fName">First Name:</label>
-        <input type="text" id="fname" name="firstName" style="width: 310px;"><br><br>
-        <label for="lastName">Last Name:</label>
-        <input type="text" name="lastName" style="width: 310px;"><br><br>
-        <label for="email">Email:</label>
-        <input type="text" name="emailAddress" style="width: 340px;"><br><br>
-        <!--This is the section for the drop down input
+    <div class="centerForm" style="text-align: center;">
+      <div class="containOutput">
+        <div class="centerContainedOutput">
+          <h1>Reserve A Room Below</h1>
+          <form action="roomResData.php">
+            <!--This is the section for the form input
               that will be sent to a page called
               roomResData.php--->
-        <label for="TimeSlot">Choose a time:</label>
-        <select name = "time" id="times">
-          <option value="8:00AM-9:00AM">8:00AM-9:00AM</option>
-          <option value="9:00AM-10:00AM">9:00AM-10:00AM</option>
-          <option value="10:00AM-11:00AM">10:00AM-11:00AM</option>
-          <option value="11:00AM-12:00PM">11:00AM-12:00PM</option>
-          <option value="1:00PM-2:00PM">12:00PM-1:00PM</option>
-          <option value="1:00PM-2:00PM">1:00PM-2:00PM</option>
-          <option value="2:00PM-3:00PM">2:00PM-3:00PM</option>
-          <option value="3:00PM-4:00PM">3:00PM-4:00PM</option>
-          <option value="5:00PM-6:00PM">5:00PM-6:00PM</option>
-          <option value="6:00PM-7:00PM">6:00PM-7:00PM</option>
-          <option value="7:00PM-8:00PM">7:00PM-8:00PM</option>
-          <option value="8:00PM-9:00PM">8:00PM-9:00PM</option>
-          <option value="9:00PM-10:00PM">9:00PM-10:00PM</option>
-          <option value="10:00PM-11:00PM">10:00PM-11:00PM</option>
-          <option value="11:00PM-12:00PM">11:00PM-12:00PM</option>
-        </select><br><br>
-          <input type="submit" name="submit" value="Submit"><br><br>
-
-      </form>
+            <?php
+              if(isset($_GET['roomSelection']))
+              {
+                $roomSelected = $_GET['roomType'];
+                $location  = $_GET['building'];
+                $numOfLocation = $_GET['roomNum'];
+              }
+            echo "<label for=\"roomSel\" class = \"redFont\">Room Selected: $roomSelected $location $numOfLocation</label>";
+            ?>
+            <br>
+            <label for="fName">First Name:</label>
+            <input type="text" id="fname" name="firstName" style="width: 310px;"><br><br>
+            <label for="lastName">Last Name:</label>
+            <input type="text" name="lastName" style="width: 310px;"><br><br>
+            <label for="email">Email:</label>
+            <input type="text" name="emailAddress" style="width: 340px;"><br><br>
+            <!--This is the section for the drop down input
+                  that will be sent to a page called
+                  roomResData.php--->
+            <label for="TimeSlot">Choose a time:</label>
+            <select name = "time" id="times">
+              <option value="8:00AM-9:00AM">8:00AM-9:00AM</option>
+              <option value="9:00AM-10:00AM">9:00AM-10:00AM</option>
+              <option value="10:00AM-11:00AM">10:00AM-11:00AM</option>
+              <option value="11:00AM-12:00PM">11:00AM-12:00PM</option>
+              <option value="1:00PM-2:00PM">12:00PM-1:00PM</option>
+              <option value="1:00PM-2:00PM">1:00PM-2:00PM</option>
+              <option value="2:00PM-3:00PM">2:00PM-3:00PM</option>
+              <option value="3:00PM-4:00PM">3:00PM-4:00PM</option>
+              <option value="5:00PM-6:00PM">5:00PM-6:00PM</option>
+              <option value="6:00PM-7:00PM">6:00PM-7:00PM</option>
+              <option value="7:00PM-8:00PM">7:00PM-8:00PM</option>
+              <option value="8:00PM-9:00PM">8:00PM-9:00PM</option>
+              <option value="9:00PM-10:00PM">9:00PM-10:00PM</option>
+              <option value="10:00PM-11:00PM">10:00PM-11:00PM</option>
+              <option value="11:00PM-12:00PM">11:00PM-12:00PM</option>
+            </select><br><br>
+            <input type="submit" name="submit" value="Submit"><br><br>
+            <?php
+              echo "
+              <form action=\"roomResData.php\">
+                <input type=\"hidden\" name=\"loc\" value=\"$roomSelected\">
+              </form>";
+            ?>
+          </form>
+        <form action="roomDisplay.php">
+            <input type="submit" name="chooseAgain" value="Choose Another Room">
+        </form>
+        </div>
+      </div>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
--- reservationweb/htdocs/pages/roomResData.php
+++ reservationweb/htdocs/pages/roomResData.php
@@ -1,18 +1,19 @@
 <?php
-	include_once ("connect.php");
+	include_once ('connect.php');
 
 	$nullEntryPresent = false;
 	$firstName =NULL;
 	$lastName =NULL ;
 	$email=NULL;
 	$timeSlot=NULL;
-	$room = "Room Test";
+	$room = NULL;
 	if(isset($_GET['submit']))
 	{
 		$firstName = $_GET['firstName'];
 		$lastName = $_GET['lastName'];
 		$email = $_GET['emailAddress'];
 		$timeSlot =  $_GET['time'];
+		$room = $_GET['loc'];
 		if($firstName == NULL || $lastName == NULL || $email == NULL ||$timeSlot == NULL)
 		{
 			$nullEntryPresent = true;
@@ -30,7 +31,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-    <title>Confirmation</title>
+  <title>Confirmation</title>
   </head>
 
   <style type="text/css">
@@ -38,53 +39,71 @@
   	{
   		text-align: center;
   	}
+
+  	body
+  	{
+  		background-color: #1C4F9C;
+  	}
+  	.containOutput
+  	{
+		margin: auto;
+		width: 30%;
+  	}
+  	.centerContainedOutput
+  	{
+  		margin: 10px;
+  		border: 5px solid #1C5438;
+  		padding: 10px;
+  		background-color: white;
+  		width: 550px;
+  		height: 550px;
+  		text-align: center;
+  	}
   </style>
+
+  <?php
+    include_once('default.php');
+    defaultHeader();
+  ?>
+
   <body>
-    <h1>
-    	<?php
-	    	if($nullEntryPresent != true)
-	    	{
-		    	echo "You, $firstName $lastName, have a reservation at $timeSlot\n";
-		    	echo "for a $room";
-		    }
-		    else
-		    {
-		    	echo "Please ensure all forms are filled out!";
-		    }
+  	<div class ="containOutput">
+  		<div class="centerContainedOutput">
+		    <h1>
+		    	<?php
+			    	if($nullEntryPresent != true)
+			    	{
+				    	echo "You, $firstName $lastName, have a reservation at $timeSlot\n";
+				    	echo "for a $room";
+				    	echo "<br>";
+				    }
+				    else
+				    {
+				    	echo "Please ensure all forms are filled out!";
+				    }
 
-		    $connect = connectDB();
-			
-	    $insertUserData = "INSERT INTO userdata (firstName,lastName,email,time) VALUES ('$firstName','$lastName','$email','$timeSlot')";
+				    $connect = connectDB();
+					
+			    /*$insertUserData = "INSERT INTO user (email,time) VALUES ('$email','$timeSlot')";
 
-		if($connect->query($insertUserData) == TRUE)
-		{
-			echo "DATA inserted";
-		}
-		else
-		{
-			echo "data insertion failed";
-			echo "<br>";
-			echo "Error: " .$insertUserData."<br>".$connect->error;
-		}
-    	?>
+				if($connect->query($insertUserData) == TRUE)
+				{
+					echo "<br>";
+					echo "Thank you!";
+				}
+				else
+				{
+					echo "data insertion failed";
+					echo "<br>";
+					echo "Error: " .$insertUserData."<br>".$connect->error;
+				}
+		    	*/?>
 
-
-
-
-    	<form action="roomRes.php">
-    		<input type="submit" value="Back to form">
-    	</form>
-    </h1>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
-    -->
+		    	<form action="roomRes.php">
+		    		<input style = "margin: 100px; border: 3px solid black;" type="submit" value="Back to form">
+		    	</form>
+	    	</h1>
+    	</div>
+    </div>
   </body>
 </html>
