008ca04aa0512b6ac80cf08af0a1268bc65924f7
TerraReef27 <terrareef27@gmail.com>
2021-03-14 15:04:34 -0400

    Updated Header
    Header is now more flexible to size changes

---
+++ reservationweb/htdocs/pages/roomaval.php
@@ -0,0 +1,24 @@
+<?php
+    include_once('../php/default.php');
+    require_once('../php/checksession.php');
+    defaultHeader();
+?>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<title>Room Reservation</title>
+</head>
+<body>
+	<div class="container-fluid">
+		test
+		<form>
+
+		</form>
+	</div>
+</body>
+</html>
