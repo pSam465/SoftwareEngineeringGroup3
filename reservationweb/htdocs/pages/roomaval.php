350484a04c8d49a68cf861e66c3d2243634a109c
TerraReef27 <terrareef27@gmail.com>
2021-03-14 15:37:08 -0400

    Page Layout
    laid out the page for inserting the data into

--- reservationweb/htdocs/pages/roomaval.php
+++ reservationweb/htdocs/pages/roomaval.php
@@ -10,15 +10,29 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="../css/reserve.css">
 
 	<title>Room Reservation</title>
 </head>
 <body>
 	<div class="container-fluid">
-		test
-		<form>
+		<div class="row">
+			<div class="col d-flex justify-content-center content-block">
 
-		</form>
+			</div>
+			<div class="col">
+				<div class="row">
+					<div class="col d-flex justify-content-center content-block">
+						test a
+					</div>
+				</div>
+				<div class="row">
+					<div class="col d-flex justify-content-center content-block">
+						test b
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
