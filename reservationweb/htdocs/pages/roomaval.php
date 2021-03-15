c62a8f316148bd9726596f0aac1d13fa59e14fd6
TerraReef27 <terrareef27@gmail.com>
2021-03-15 00:32:25 -0400

    Get Rooms
    Page now gets and displays rooms from database

--- reservationweb/htdocs/pages/roomaval.php
+++ reservationweb/htdocs/pages/roomaval.php
@@ -1,7 +1,45 @@
 <?php
     include_once('../php/default.php');
     require_once('../php/checksession.php');
+    require_once("../php/sqlSts.php");
     defaultHeader();
+
+    function showrooms($query)
+    {
+    	$conn = connectDB();
+		if(!$conn)
+		{
+			exit("Unable to connect to DB");
+		}
+		$result = $conn->query($query);
+		if(!$result) die("Error.");
+		$rows=$result->num_rows;
+		if($rows>0)
+		{
+			echo<<<_END
+			<div class="container">
+			<table class="table table-fixed">
+			<thead><th scope="col">Room</th><thead>
+			<tbody>
+			_END;
+			for($i=0; $i<$rows; $i++)
+			{
+				$row = $result->fetch_array(MYSQLI_ASSOC);
+				$room = $row['building'] . " " . $row['roomNum'];
+
+				echo<<<_END
+				<tr>
+				<td>$room</td>
+				</tr>
+				_END;
+			}
+			echo "</tbody></table></div>";
+		}
+		else
+		{
+			echo "No rooms available";
+		}
+    }
 ?>
 
 <html lang="en">
@@ -18,7 +56,10 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col d-flex justify-content-center content-block">
-
+				<?php
+					$askrooms = "SELECT * FROM `room`";
+					showrooms($askrooms);
+				?>
 			</div>
 			<div class="col">
 				<div class="row">
