93854f38d7eea7d92ff052852704f705ae15c4b3
TerraReef27 <terrareef27@gmail.com>
2021-03-17 23:51:56 -0400

    Room Time Search
    The find room will now pull up a list of rooms that do not overlap in time

--- reservationweb/htdocs/pages/roomaval.php
+++ reservationweb/htdocs/pages/roomaval.php
@@ -1,44 +1,77 @@
 <?php
-    include_once('../php/default.php');
-    require_once('../php/checksession.php');
-    require_once("../php/sqlSts.php");
-    defaultHeader();
+include_once('../php/default.php');
+require_once('../php/checksession.php');
+require_once("../php/sqlSts.php");
+defaultHeader();
 
-    function showrooms($query)
-    {
-    	$conn = connectDB();
-		if(!$conn)
+$date = $starttime = $endtime = $askquery = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+	global $date;
+	global $starttime;
+	global $endtime;
+	global $askquery;
+	
+	//Set all of the submitted variales
+	if(!empty($_POST['date']))
+	{
+		$date = $_POST['date'];
+	}
+	if(!empty($_POST['starttime']))
+	{
+		$starttime = $_POST['starttime'];
+	}
+	if(!empty($_POST['endtime']))
+	{
+		$endtime = $_POST['endtime'];
+	}
+	if(!empty($_POST['askquery']))
+	{
+		$askquery = $_POST['askquery'];
+	}
+}
+
+function showrooms()
+{
+	$query = generatequery(); 
+	$conn = connectDB();
+	if(!$conn)
+	{
+		exit("Unable to connect to DB");
+	}
+	$result = $conn->query($query);
+	if(!$result) die("Error.");
+	$rows=$result->num_rows;
+	if($rows>0)
+	{
+		echo "<tbody>";
+		for($i=0; $i<$rows; $i++)
 		{
-			exit("Unable to connect to DB");
-		}
-		$result = $conn->query($query);
-		if(!$result) die("Error.");
-		$rows=$result->num_rows;
-		if($rows>0)
-		{
+			$row = $result->fetch_array(MYSQLI_ASSOC);
+			$room = $row['building'] . " " . $row['roomNum'];
+
 			echo<<<_END
-			<table class="table table-striped table-fixed">
-			<thead><th scope="col">Room</th><thead>
-			<tbody>
+			<tr>
+			<td>$room</td>
+			</tr>
 			_END;
-			for($i=0; $i<$rows; $i++)
-			{
-				$row = $result->fetch_array(MYSQLI_ASSOC);
-				$room = $row['building'] . " " . $row['roomNum'];
+		}
+		echo "</tbody>";
+	}
+}
 
-				echo<<<_END
-				<tr>
-				<td>$room</td>
-				</tr>
-				_END;
-			}
-			echo "</tbody></table>";
-		}
-		else
-		{
-			echo "No rooms available";
-		}
-    }
+function generatequery()
+{
+	global $date;
+	global $starttime;
+	global $endtime;
+
+	$startdate = $date." ".$starttime.":00";
+	$enddate = $date." ".$endtime.":00";
+	$query = "SELECT * FROM room JOIN roomreservation ON room.roomID=roomreservation.roomID WHERE NOT '$startdate' <= roomreservation.reservationEnd OR NOT '$enddate' >= roomreservation.reservationStart;";
+	return $query;
+}
 ?>
 
 <html lang="en">
@@ -56,52 +89,63 @@
 		<div class="row">
 			<div class="col-md content-block">
 				<div class="row">
-					<div class="col-sm d-flex justify-content-center">
+					<div class="col-sm d-flex justify-content-center">	
 						<input type="text" type="form-control" name="roomsearch" id="roomsearch" placeholder="Search for a room" style="width: 100%;">
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-sm d-flex justify-content-center">
 					<div class="container scrollable">
-					<?php
-						$askrooms = "SELECT * FROM `room`";
-						showrooms($askrooms);
-					?>
+						<table class="table table-hover table-fixed" id="roomtable">
+							<thead><th scope="col">Room</th><thead>
+							<?php
+								showrooms();
+							?>
+						</table>
 					</div>
 					</div>
 				</div>
 			</div>
 			<div class="col-md">
 				<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" autocomplete="off" />
+				<div class="content-block">
 				<div class="row">
-					<div class="col d-flex justify-content-center content-block">
+					<div class="col d-flex justify-content-center">
 						<div class="align-self-center">
 							<h3>Select a Date</h3>
 						</div>
 						<div class="form-group">
-							<input type="date" class="form-control" id="date" name="date" value="<?php echo getdate() ?>" />
-						</div>		
+							<input type="date" class="form-control" id="date" name="date" value="<?php echo $date ?>" />
+						</div>
 					</div>
 				</div>
+				<hr class="rounded">
 				<div class="row">
-					<div class="col d-flex justify-content-center content-block">
+					<div class="col d-flex justify-content-center">
 						<div class="align-self-center">
 							<h3>Select a Time</h3>
 						</div>
-						<div class="form-group">
-							Start<input type="time" class="form-control" id="starttime" name="starttime" value="<?php echo gettimeofday() ?>" />
+						<div class="form-group">	
+							Start<input type="time" class="form-control" id="starttime" name="starttime" value="<?php echo $starttime ?>" />
 						</div>
 						<div class="form-group">
-							End<input type="time" class="form-control" id="endtime" name="endtime" value="<?php echo gettimeofday() ?>" />
-						</div>	
+							End<input type="time" class="form-control" id="endtime" name="endtime" value="<?php echo $endtime ?>" />
+						</div>
 					</div>
 				</div>
+				<hr class="rounded">
 				<div class="row">
 					<div class="col d-flex justify-content-center">
-						<button type="submit" class="btn btn-primary btn-lg btn-block">Find Avalible Rooms</button>
+						<button type="submit" class="btn btn-outline-info btn-lg btn-block">Find a Room</button>
 					</div>
 				</div>
+				</div>
 				</form>
+				<div class="row">
+					<div class="col d-flex justify-content-center">
+						<button type="submit" class="btn btn-primary btn-lg btn-block">Reserve Room</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
