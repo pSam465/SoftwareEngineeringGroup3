dcf6e663507bc25f18be53cdae9c20dc80c003fe
TerraReef27 <terrareef27@gmail.com>
2021-03-31 20:42:00 -0400

    Implementation Stuff
    In the middle of implementation

--- reservationweb/htdocs/pages/resdetails.php
+++ reservationweb/htdocs/pages/resdetails.php
@@ -4,7 +4,7 @@ require_once('../php/checksession.php');
 require_once("../php/connect.php");
 defaultHeader();
 
-$room = $starttime = $endtime = "";
+$room = $starttime = $endtime = $repeattype = $endrepeat = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
@@ -15,20 +15,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 	if(isset($_POST['room']))
 	{
 		$room = $_POST['room'];
+		echo $room;
 	}
 	if(isset($_POST['starttime']))
 	{
 		$starttime = $_POST['starttime'];
+		echo $starttime;
 	}
 	if(isset($_POST['endtime']))
 	{
 		$endtime = $_POST['endtime'];
+		echo $endtime;
 	}
-}
-
-function createreservation()
-{
-
 }
 
 ?>
@@ -45,28 +43,59 @@ function createreservation()
 </head>
 <body>
 <div class="container-fluid">
-	<div class="col content-block">
-		<div>
-			<h3>Reservation Details<h3>
-				<div class="content-block">
-					<p><h5>Room: </h5><?php echo $room; ?></p>
-					<p><h5>Start Time: </h5><?php echo $starttime; ?></p>
-					<p><h5>End Time: </h5><?php echo $endtime; ?></p>
+	<div class="row">
+		<div class="col-sm content-block m-2">
+			<div>
+				<h3>Reservation Details</h3>
+					<div>
+						<p><h5>Room: </h5><?php echo $room; ?></p>
+						<p><h5>Start Time: </h5><?php echo $starttime; ?></p>
+						<p><h5>End Time: </h5><?php echo $endtime; ?></p>
+					</div>
+			</div>
+		</div>
+		<div class="col-sm content-block m-2">
+			<form method="POST" action="../php/formsubmit" autocomplete="off" >
+			<div class="form-group">
+				<h4>Repeat Event</h4>
+				<div>
+					<select class="form-control" id="repeattype" onchange="checkSelected()">
+						<option value="0">Do not repeat</option>
+						<option value="1">Repeat Daily</option>
+						<option value="2">Repeat Weekly</option>
+						<option value="3">Repeat Monthly</option>
+						<option value="4">Repeat Yearly</option>
+					</select>
+					<div id="repeatcal" style="display:none;">
+						<h5>Repeat Until:</h5>
+						<input type="date" class="form-control" id="repeatend" name="repeatend" />
+					</div>
 				</div>
+				<button type="submit" class="btn btn-primary btn-lg btn-block form-control mt-3">Reserve Room</button>
+			</div>
+				<input type="hidden" name="room" value="<?php global $room; echo $room; ?>">
+				<input type="hidden" name="starttime" value="<?php global $starttime; echo $starttime; ?>">
+				<input type="hidden" name="endtime" value="<?php global $endtime; echo $endtime; ?>">
+			</form>
 		</div>
-		<form>
-		<div class="form-group">
-			<h3>Repeat Event</h3>
-			<select class="form-control">
-				<option>Do not repeat</option>
-				<option>Repeat Daily</option>
-				<option>Repeat Weekly</option>
-				<option>Repeat Monthly</option>
-			</select>
-			<button type="submit" class="btn btn-primary btn-lg btn-block form-control mt-3">Reserve Room</button>
-		</div>
-		</form>
 	</div>
 </div>
 </body>
+
+<script type="text/javascript">
+function checkSelected()
+{
+	var repeat = document.getElementById("repeattype").value;
+	var cal = document.getElementById("repeatcal");
+	if(repeat > 0)
+	{
+		cal.style.display = "";
+	}
+	else
+	{
+		cal.style.display = "none";
+	}
+}
+</script>
+
 </html>
